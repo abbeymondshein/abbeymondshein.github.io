a144b14c83ad3fb04282e3f5e56499720be57058
Abbey Mondshein <killture@gmail.com>
2018-04-05 20:28:46 -0400

    upload wishlist

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 /* Exercise 1: Wish list */
+function addToList(item) {
+	$('#items').append('<li>' + item + '<span class="label pending">Pending</span></li>')
+	$('span').css('class', 'label pending')
+}
+
+$(document).on('click', '#add-to-list', function() {
+	var item = $('#item');
+	console.log(item.val())
+	addToList(item.val())
+	item.val(" ")
+	item.focus()
+	updateTotal()
+
+	$('.pending').click(function() {
+	var li_node = $(this).parent();
+	li_node.append("<span class='label success'>Done!</span>")
+	$(this).remove()
+	li_node.addClass('completed');
+	updateTotal()
+	})
+
+	
+});
+
+function updateTotal() {
+	var pending = $('.pending').length
+	var completed = $('.completed').length
+	$('.total').text('Pending: ' + pending + ' Completed: ' + completed)
+}
